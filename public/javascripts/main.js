76415ba45eeba58bdee9e99254e9fd3b953c8695
takamasa suzuki <midgard10257@gmail.com>
2016-06-01 14:36:33 +0900

    余計なもの削除

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,11 +3,11 @@ var socket = io.connect();
 
 var create_chat_html = function(msg){
   var chat = $('<div>');
-  if(msg.user.match(/^[a-zA-Z0-9_]+$/)){
-    chat.append( $('<span>').addClass('icon').html(
-      $('<img>').attr('src', 'http://twiticon.herokuapp.com/'+msg.user)
-    ) );
-  }
+  // if(msg.user.match(/^[a-zA-Z0-9_]+$/)){
+  //   chat.append( $('<span>').addClass('icon').html(
+  //     $('<img>').attr('src', 'http://twiticon.herokuapp.com/'+msg.user)
+  //   ) );
+  // }
   chat.append( $('<span>').addClass('user').text(msg.user) );
   chat.append(' : ');
   chat.append( $('<span>').addClass('body').html(msg.body.markup()) );
@@ -15,43 +15,27 @@ var create_chat_html = function(msg){
 };
 
 var Tiqav = new function(){
-  var self = this;
+  // var self = this;
+  //
+  // this.cache = {};
 
-  this.cache = {};
-
-  this.search = function(word){
-    if(word === null || word === ''){
-      $('#tiqav').html('');
-      return;
-    }
-    if(self.cache[word] === undefined){
-      $.getJSON("http://api.tiqav.com/search.json?q="+word+"&callback=?", function(res){
-        self.cache[word] = res;
-        self.on_search(res);
-      });
-    }
-    else{
-      self.on_search(self.cache[word]);
-    }
-  };
-
-  this.on_search = function(data){
-    $('#tiqav').html('');
-    var max_images = data.length > 6 ? 6 : data.length;
-    for(var index = 0; index <  max_images; index++){
-      (function(){
-        var i = data[index];
-        var img_url = 'http://img.tiqav.com/'+i.id+'.th.'+i.ext;
-        var img = $('<img>').
-          attr('src', img_url).
-          click(function(e){
-            $('#body').val(img_url).focus();
-            self.search(null);
-          });
-        $('#tiqav').prepend($('<li>').html(img));
-      })();
-    }
-  };
+  // this.search = function(word){
+  //   if(word === null || word === ''){
+  //     return;
+  //   }
+  //   if(self.cache[word] === undefined){
+  //     $.getJSON("http://api.tiqav.com/search.json?q="+word+"&callback=?", function(res){
+  //       self.cache[word] = res;
+  //       self.on_search(res);
+  //     });
+  //   }
+  //   else{
+  //     self.on_search(self.cache[word]);
+  //   }
+  // };
+  //
+  // this.on_search = function(data){
+  // };
 }();
 
 var chat_post = function(e){
@@ -63,13 +47,13 @@ var chat_post = function(e){
 $(function(){
   var sid = null;
   $('#body').keyup(function(e){
-    clearTimeout(sid);
-    sid = setTimeout(function(){
-      Tiqav.search($('#body').val());
-    }, 300);
+    // clearTimeout(sid);
+    // sid = setTimeout(function(){
+    //   Tiqav.search($('#body').val());
+    // }, 300);
   });
   $('#body').bind('webkitspeechchange', function(e){
-    Tiqav.search($('#body').val());
+    // Tiqav.search($('#body').val());
   });
   $('#body').keydown(function(e){
     if(e.keyCode === 13){
